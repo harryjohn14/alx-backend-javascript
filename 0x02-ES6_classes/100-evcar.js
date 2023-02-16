779b9d52b2f40a67d5fffafb188a95dc2c5f0fd8
Harrison John <ugochukwujohn2014@gmail.com>
2023-02-16 11:25:00 -0800

    Implement a class named EVCar that extends the Car class

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,26 @@
-/* eslint-disable */
+/* eslint-disable no-underscore-dangle */
 import Car from './10-car';
 
 export default class EVCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-		this._range = range;
-	}
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
 
-	cloneCar() {
-		return new Car();
-	}
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
 }
