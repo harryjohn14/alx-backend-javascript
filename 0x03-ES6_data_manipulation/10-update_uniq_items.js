8c2026ce4e14982aea5c744d3f4e8bed3118d998
Harrison John <ugochukwujohn2014@gmail.com>
2023-02-18 05:28:21 -0800

    Create a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,16 +1,10 @@
-const groceriesList = () => {
-  const res = new Map();
-  const objetos = {
-    Apples: 10,
-    Tomatoes: 10,
-    Pasta: 1,
-    Rice: 1,
-    Banana: 5,
-  };
-  for (const key of Object.keys(objetos)) {
-    res.set(key, objetos[key]);
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
-  return res;
-};
-
-export default groceriesList;
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
